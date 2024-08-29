eb3563df3841c5a25a5ca837c7225b8efe44017a
guskirb <anguskirby1998@gmail.com>
2024-08-29 19:20:27 +0100

    TEST: Remove BG

--- src/components/ui/hero/hero.tsx
+++ src/components/ui/hero/hero.tsx
@@ -2,13 +2,13 @@ import { Link } from "react-scroll";
 
 import MotionDiv from "../motion-div";
 import { FlipWords } from "./flip-words";
-import { HeroBg } from "./hero-bg";
+// import { HeroBg } from "./hero-bg";
 
 export default function Hero() {
   return (
     <div className="flex flex-col duration-700 h-full">
       <div className="w-full h-full relative overflow-hidden flex justify-center items-center">
-        <HeroBg />
+        {/* <HeroBg /> */}
         <MotionDiv>
           <div className="pointer-events-none dark:text-white z-10 flex flex-col skew-y-[19deg] duration-700 text-xs lg:text-xl">
             <h1>
