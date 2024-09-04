a75366c7f2f582ee66a0b58415caa891e9053bca
guskirb <anguskirby1998@gmail.com>
2024-09-04 03:47:29 +0100

    Remove lazy loading

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,19 +1,12 @@
 import { Element } from "react-scroll";
-import React from "react";
 
 import Hero from "../../components/ui/hero/hero";
 import NavBar from "../../components/ui/nav/nav-bar";
+import Skills from "../../components/ui/skills/skills";
 import ThemeButton from "../../components/ui/theme-button/theme-button";
-// import Skills from "../../components/ui/skills/skills";
-// import Projects from "../../components/ui/projects/projects";
-// import About from "../../components/ui/about/about";
-// import Footer from "../../components/ui/footer/footer";
-const Skills = React.lazy(() => import("../../components/ui/skills/skills"));
-const Projects = React.lazy(
-  () => import("../../components/ui/projects/projects")
-);
-const About = React.lazy(() => import("../../components/ui/about/about"));
-const Footer = React.lazy(() => import("../../components/ui/footer/footer"));
+import Projects from "../../components/ui/projects/projects";
+import About from "../../components/ui/about/about";
+import Footer from "../../components/ui/footer/footer";
 
 export default function Home() {
   return (
@@ -34,7 +27,7 @@ export default function Home() {
         <Projects />
       </Element>
       <Element name="contact">
-        <Footer />
+      <Footer />
       </Element>
       <ThemeButton />
       <NavBar />
