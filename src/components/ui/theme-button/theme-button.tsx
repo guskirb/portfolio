47141204f1d09fdb39bef2d1b196d31953684110
guskirb <anguskirby1998@gmail.com>
2024-09-06 02:14:44 +0100

    Add animation toggle button

--- src/components/ui/theme-button/theme-button.tsx
+++ src/components/ui/theme-button/theme-button.tsx
@@ -1,11 +1,10 @@
-
 import useTheme from "../../../hooks/useTheme";
 
 export default function ThemeButton() {
-  const { darkMode, setDarkMode } = useTheme();
+  const { darkMode, setDarkMode, animation, setAnimation } = useTheme();
 
   return (
-    <div className="fixed z-20 top-5 right-5">
+    <div className="fixed z-20 top-5 right-5 flex flex-col gap-3">
       <label
         className="themeContainer"
         title={darkMode ? "Activate light mode" : "Activate dark mode"}
@@ -17,6 +16,16 @@ export default function ThemeButton() {
           onChange={() => setDarkMode(!darkMode)}
         />
         <div />
+      </label>
+      <label className="switch">
+        <input
+          type="checkbox"
+          defaultChecked={animation}
+          onChange={() => setAnimation(!animation)}
+        />
+        <div>
+          <span></span>
+        </div>
       </label>
     </div>
   );
