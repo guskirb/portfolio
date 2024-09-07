484b6dfce407b1ce77818e4670dfdfd88c14345d
guskirb <anguskirby1998@gmail.com>
2024-09-07 11:24:13 +0100

    Set animation disabled card component settings

--- src/components/ui/skills/card.tsx
+++ src/components/ui/skills/card.tsx
@@ -20,7 +20,7 @@ export default function Card({
   return (
     <div className="hover:-translate-y-3 duration-300">
       <motion.div
-        initial={animation ? { opacity: 0, y: -20 } : {}}
+        initial={animation ? { opacity: 0, y: -20 } : { opacity: 1, y: 0 }}
         whileInView={
           animation
             ? {
@@ -28,7 +28,10 @@ export default function Card({
                 y: 0,
                 transition: { delay: 0.2 * index, duration: 0.5 },
               }
-            : {}
+            : {
+                opacity: 1,
+                transition: { delay: 0, duration: 0 },
+              }
         }
         viewport={animation ? { once: true, amount: 0.5 } : {}}
         className="skill-card text-white dark:text-[#262626] dark:bg-neutral-100 bg-neutral-900 rounded-3xl p-8 flex flex-col gap-5"
