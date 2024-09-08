14346cae5d20446e1ad83257894a9ab6bd0c2458
guskirb <anguskirby1998@gmail.com>
2024-09-08 05:21:00 +0100

    Conditionally render hero animations

--- src/components/ui/hero/hero.tsx
+++ src/components/ui/hero/hero.tsx
@@ -5,9 +5,11 @@ import { FlipWords } from "./flip-words";
 import { HeroBg } from "./hero-bg";
 import { useEffect, useState } from "react";
 import { HeroBgStatic } from "./hero-bg-static";
+import useTheme from "../../../hooks/useTheme";
 
 export default function Hero() {
   const [width, setWidth] = useState<number>(window.innerWidth);
+  const { animation } = useTheme();
 
   function handleWindowSizeChange() {
     setWidth(window.innerWidth);
@@ -22,14 +24,21 @@ export default function Hero() {
   return (
     <div className="flex flex-col duration-700 h-full">
       <div className="w-full h-full relative overflow-hidden flex justify-center items-center">
-        {width > 800 ? <HeroBg /> : <HeroBgStatic />}
+        {width > 800 && animation ? <HeroBg /> : <HeroBgStatic />}
         <MotionDiv>
           <div className="pointer-events-none dark:text-white z-10 flex flex-col skew-y-[19deg] duration-700 text-xs lg:text-xl">
             <h1>
               Hi!<span className="wave">👋</span>I'm Gus,
             </h1>
             <h1>
-              a<FlipWords words={["full-stack", "front-end", "back-end"]} />
+              a
+              {animation ? (
+                <FlipWords words={["full-stack", "front-end", "back-end"]} />
+              ) : (
+                <p className="inline-block whitespace-nowrap px-2z-10 relative text-left text-sky-500 font-medium px-2">
+                  full-stack
+                </p>
+              )}
             </h1>
             <h1>web developer.</h1>
           </div>
