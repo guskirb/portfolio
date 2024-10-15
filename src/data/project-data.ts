090b5c55348c52c59cec8ac2754340d1474b74b0
guskirb <anguskirby1998@gmail.com>
2024-10-15 06:55:40 +0100

    Replace project vid urls

--- src/data/project-data.ts
+++ src/data/project-data.ts
@@ -6,7 +6,7 @@ export const projectData = [
     description:
       "A social media application made using React and Tailwind on the front end. The back end is a REST API built with Node.js and Express, using Json Web Tokens for authentication.",
     video:
-      "https://live.staticflickr.com/video/53991903473/a99245b8be/720p.mp4?s=eyJpIjo1Mzk5MTkwMzQ3MywiZSI6MTcyNjU5NTU0OCwicyI6ImNlNDQ2ZDA4MTU5YTlhOTQ0YzYyZjlhZmUxYjQ4ZGE2N2EyN2VmMmIiLCJ2IjoxfQ",
+      "https://res.cloudinary.com/dxvkpbvgr/video/upload/v1728971557/social-media_hfghpu.mp4",
     stack: [
       "typescript",
       "react",
@@ -28,7 +28,7 @@ export const projectData = [
     description:
       "A messaging application developed using React and Tailwind for the UI, featuring a REST API built with Node.js and Express, which queries a MongoDB database.",
     video:
-      "https://live.staticflickr.com/video/53991903598/d938990ea2/720p.mp4?s=eyJpIjo1Mzk5MTkwMzU5OCwiZSI6MTcyNjU5NTYxMCwicyI6ImIzZmFiZjFmNDgwZTdhMTc3NTUwZjIxNTkyODk2M2QxZjRkZDM0MGIiLCJ2IjoxfQ",
+      "https://res.cloudinary.com/dxvkpbvgr/video/upload/v1728971552/chatroom_gmqjie.mp4",
     stack: ["typescript", "css", "react", "node", "express", "mongodb", "vite"],
     preview: "https://chat-app-guskirb.netlify.app/",
     front: "https://github.com/guskirb/messaging-app-clientTOP",
@@ -41,7 +41,7 @@ export const projectData = [
     description:
       "A blogging application developed with React, Node.js, and Express. The backend features a JWT authentication REST API that connects to a MongoDB database.",
     video:
-      "https://live.staticflickr.com/video/53991983799/f4f6a1048d/720p.mp4?s=eyJpIjo1Mzk5MTk4Mzc5OSwiZSI6MTcyNjU5NTYyOCwicyI6IjY0MGZhYTZkMGNhMzEwOTRkNDkyYTMyZGViMDE4MTQ4YmI0NjE2ZjYiLCJ2IjoxfQ",
+      "https://res.cloudinary.com/dxvkpbvgr/video/upload/v1728971566/blog_w7veam.mp4",
     stack: ["typescript", "css", "react", "node", "express", "mongodb", "vite"],
     preview: "https://main--guskirb-blog.netlify.app/",
     front: "https://github.com/guskirb/blog-clientTOP",
@@ -54,7 +54,7 @@ export const projectData = [
     description:
       "A 'Where's Wally' photo-tagging game, featuring a React front end and an Express, Node.js back end designed to manage leaderboard information and character coordinates.",
     video:
-      "https://live.staticflickr.com/video/53991983729/a0b46ce680/720p.mp4?s=eyJpIjo1Mzk5MTk4MzcyOSwiZSI6MTcyNjU5NTY0OSwicyI6ImUwNDhhOWQ2OTRiN2NiMDc5ZTUwMzFlNGFkNzI4NTc1YjhlM2M2MjgiLCJ2IjoxfQ",
+      "https://res.cloudinary.com/dxvkpbvgr/video/upload/v1728971578/wheres-wally_o3bxmc.mp4",
     stack: [
       "typescript",
       "css",
@@ -76,7 +76,7 @@ export const projectData = [
     description:
       "An application that uses EJS for server-side rendering and manages data from a MongoDB database, using Node.js and Express.",
     video:
-      "https://live.staticflickr.com/video/53992096105/22f93e1bc4/720p.mp4?s=eyJpIjo1Mzk5MjA5NjEwNSwiZSI6MTcyNjU5NTY2NCwicyI6ImM1MDEwYWEzMjNiNmI4ZTE5Y2JhMDc4NmU5NGU0M2Y5ZGEyOTlkYjYiLCJ2IjoxfQ",
+      "https://res.cloudinary.com/dxvkpbvgr/video/upload/v1728971557/inventory_kbtmbp.mp4",
     stack: ["html", "css", "javascript", "express", "mongodb"],
     preview: "https://quartz-holly-knot.glitch.me/",
     back: "https://github.com/guskirb/inventory-applicationTOP",
@@ -88,7 +88,7 @@ export const projectData = [
     description:
       "A shopping cart React application that fetches data from a fake store API. Allows you to filter results, add and remove items from your cart and checkout.",
     video:
-      "https://live.staticflickr.com/video/53991983554/c6438d39cc/720p.mp4?s=eyJpIjo1Mzk5MTk4MzU1NCwiZSI6MTcyNjU5NTY4NSwicyI6IjEyZjMyYzJiOGY3YmVmYzgwN2E5MDUwYzIwYWM1NGM5NTEzOGM4MjIiLCJ2IjoxfQ",
+      "https://res.cloudinary.com/dxvkpbvgr/video/upload/v1728971561/shopping-cart_i3vblg.mp4",
     stack: ["javascript", "css", "react", "vite", "jest"],
     preview: "https://main--strong-profiterole-d88f4a.netlify.app/",
     front: "https://github.com/guskirb/shopping-cartTOP",
@@ -100,7 +100,7 @@ export const projectData = [
     description:
       "A CV Builder React Application that allows the user to generate a CV based on inputted data. Change text and accent colours and upload a photo.",
     video:
-      "https://live.staticflickr.com/video/53991903513/b8913100a4/720p.mp4?s=eyJpIjo1Mzk5MTkwMzUxMywiZSI6MTcyNjU5NTcwNSwicyI6ImM3MWM4MGVjYmE0NTgzYWNiMTAwZjNlZTk2YjI1MzY5NmViNzcwODIiLCJ2IjoxfQ",
+      "https://res.cloudinary.com/dxvkpbvgr/video/upload/v1728971556/CV-builder_jpfodc.mp4",
     stack: ["javascript", "css", "react", "vite"],
     preview: "https://main--starlit-kulfi-eb679c.netlify.app/",
     front: "https://github.com/guskirb/CV-applicationTOP",
@@ -112,7 +112,7 @@ export const projectData = [
     description:
       "A vanilla JavaScript battleships application that allows the user to choose between two CPU difficulty settings. Unit tested using Jest.",
     video:
-      "https://live.staticflickr.com/video/53991671361/14d8c5b551/720p.mp4?s=eyJpIjo1Mzk5MTY3MTM2MSwiZSI6MTcyNjU5NTQzMiwicyI6IjY2OGZmMjAzODNhYzFjMGY1ODkwZDc1ZWU5ODNiMDIyYzE0NzMzZGIiLCJ2IjoxfQ",
+      "https://res.cloudinary.com/dxvkpbvgr/video/upload/v1728971556/battleships_gwk5ox.mp4",
     stack: ["html", "javascript", "css", "jest"],
     preview: "https://guskirb.github.io/battleshipTOP/",
     front: "https://github.com/guskirb/battleshipTOP",
@@ -124,7 +124,7 @@ export const projectData = [
     description:
       "A vanilla JavaScript weather application that retrieves data from a weather API and dynamically renders the UI based on the selected location's time and weather.",
     video:
-      "https://live.staticflickr.com/video/53991903393/d150ec26ca/720p.mp4?s=eyJpIjo1Mzk5MTkwMzM5MywiZSI6MTcyNjU5NTcyMywicyI6Ijk3ZjBlNDIyM2M0ZWMxODhhMjliZTM4N2UxOGI2ZjliMjM1NzVlOWMiLCJ2IjoxfQ",
+      "https://res.cloudinary.com/dxvkpbvgr/video/upload/v1728971551/weather-app_omoyhn.mp4",
     stack: ["html", "javascript", "css"],
     preview: "https://guskirb.github.io/weather-appTOP/",
     front: "https://github.com/guskirb/weather-appTOP",
